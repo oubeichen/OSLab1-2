956392cca9563caf63e154f3a5c77ebf77a3b7ad
oubeichen <oubeichen@gmail.com>
2013-05-13 20:59:43 +0800

    Initial vfprintf function

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -10,6 +10,13 @@ os_init(void) {
 	init_idt();
 	init_i8259();
 	printk("The OS is now working!\n");
+	/*测试字符串输出开始*/
+	int samplehex = -26;
+	char *samplestr = "The hex of -26 is:\n";
+	printk("d test:%d + %d = %d\n",-20,10,-10);
+	printk("s test and x test:%s %x\n",samplestr,samplehex);
+	printk("c test: %c is 'a' ,%c is 'c'",'a','c'); 
+	/*测试字符串输出结束*/
 	sti();
 	while (TRUE) {
 		wait_intr();
--- src/lib/vfprintf.c
+++ src/lib/vfprintf.c
@@ -12,9 +12,68 @@
 int
 vfprintf(void (*putchar_func)(char), const char *format, void **data) {
 	const char *s;
-	for (s = "please implement me\n"; *s; s ++) {
+	int charnum = 0; //记录输出字符数量
+	/*for (s = "please implement me\n"; *s; s ++) {
 		putchar_func(*s);
+	}*/
+	for(s = format; *s;s ++){
+		if(*s == '%' && *(s+1) == 'd'){
+			s++;//跳过后面的'd'
+			if(data)//以防万一data为空虽然好像没什么用因为data是void类型的没法预料
+			{
+				int *da = (int *)*(data++);
+				if(da){//以防万一da为空
+					char *str = itoa((int)da,10);//偷懒了:)
+					for(;*str;str++){//因为itoa不会返回NULL所以就直接输出
+						putchar_func(*str);
+						charnum++;
+					}
+				}
+			}
+
+		}
+		else if(*s == '%' && *(s+1) == 'x'){
+			s++;//跳过后面的'x'
+			if(data){
+				int *da = (int *)*(data++);
+				if(da){
+					char *str = itoa((int)da,16);//同样偷懒了,不用白不用:)
+					for(;*str;str++){//因为itoa不会返回NULL所以直接输出
+						putchar_func(*str);
+						charnum++;
+					}
+				}
+			}
+		}
+		else if(*s == '%' && *(s+1) == 's'){
+			s++;//跳过后面的's'
+			if(data){
+				char *da = (char *)*(data++);
+				if(da){
+					for(;*da;da++){
+						putchar_func(*da);
+						charnum++;
+					}
+				}
+
+			}
+		}
+		else if(*s == '%' && *(s+1) == 'c'){
+			s++;//跳过后面的'c'
+			if(data){
+				char da = '\0'; 
+				da = (char)(int)*(data++);//貌似要先转成int才行
+				if(da){
+					putchar_func(da);
+					charnum++;
+				}
+			}
+		}		
+		else{
+			putchar_func(*s);
+			charnum++;
+		}
 	}
-	return 0;
+	return charnum;
 }
 
